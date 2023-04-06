fcaebdc3696498db3786c8ee827ff6607d5484e7
Ash1VT <nikita.panasevitch@yandex.by>
2023-04-06 21:33:57 +0300

    commented unused staff

--- chat/src/App.js
+++ chat/src/App.js
@@ -1,25 +1,26 @@
 import React from "react";
 import useWebSocket, { ReadyState } from "react-use-websocket";
-import LoginPage from "./pages/LoginPage";
+import "./App.css"
 import ChatPage from "./pages/ChatPage";
+import LoginPage from "./pages/LoginPage";
 
 export default function App() {
-  const { readyState } = useWebSocket("ws://127.0.0.1:8000/", {   //урл бэка (ждать пока Боги сделают сервер)
-    onOpen: () => {
-      console.log("Connected!");
-    },
-    onClose: () => {
-      console.log("Disconnected!");
-    }
-  });
+  // const { readyState } = useWebSocket("ws://127.0.0.1:8000/", {   //урл бэка (ждать пока Боги сделают сервер)
+  //   onOpen: () => {
+  //     console.log("Connected!");
+  //   },
+  //   onClose: () => {
+  //     console.log("Disconnected!");
+  //   }
+  // });
 
-  const connectionStatus = {
-    [ReadyState.CONNECTING]: "Connecting",
-    [ReadyState.OPEN]: "Open",
-    [ReadyState.CLOSING]: "Closing",
-    [ReadyState.CLOSED]: "Closed",
-    [ReadyState.UNINSTANTIATED]: "Uninstantiated"
-  }[readyState];
+  // const connectionStatus = {
+  //   [ReadyState.CONNECTING]: "Connecting",
+  //   [ReadyState.OPEN]: "Open",
+  //   [ReadyState.CLOSING]: "Closing",
+  //   [ReadyState.CLOSED]: "Closed",
+  //   [ReadyState.UNINSTANTIATED]: "Uninstantiated"
+  // }[readyState];
 
   return (
     <ChatPage/>
