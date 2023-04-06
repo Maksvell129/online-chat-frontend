81a2ee4a26d9d0ab29a144316b6c052b1e6cee70
Ash1VT <nikita.panasevitch@yandex.by>
2023-04-06 21:32:49 +0300

    added boolean to choose style of message

--- chat/src/components/Message/Message.js
+++ chat/src/components/Message/Message.js
@@ -1,21 +1,27 @@
 import React from "react";
 import "../Message/Message.css";
 
-function Message({ text }) {
+function Message({ username, text, time, isOwnMessage }) {
 
     return(
         <div className="ChatRight">
-        <div className="MyMessage">
-            <div className="abel-normal-black-16px UserName">
-                Zalupa
+            <div className=
+                {
+                    isOwnMessage ? "MyMessage" : "ForeignMessage" 
+                }
+            >
+                <div className="abel-normal-black-16px UserName">
+                    {username}
+                </div>
+                
+                <div className="abel-normal-black-14px Text">
+                    {text}
+                </div>
+                
+                <div className="abel-normal-black-8px SendTime">
+                    {time}
+                </div>
             </div>
-            <div className="abel-normal-black-14px Text">
-                {text}
-            </div>
-            <div className="abel-normal-black-8px SendTime">
-                19:22
-            </div>
-        </div>
         </div>);
 }
 
