14af309292f3c2a48f919093a971de8f9fbf64aa
Ash1VT <nikita.panasevitch@yandex.by>
2023-04-13 16:53:38 +0300

    added checking token in local storage and user initialization

--- chat/src/contexts/auth/AuthProvider.js
+++ chat/src/contexts/auth/AuthProvider.js
@@ -1,23 +1,53 @@
-import React, { useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 import AuthContext from './AuthContext'
 import { getUserInformationFromAccessToken } from '../../utils/user'
+import AuthService from '../../services/AuthService'
+import { getAccessToken } from '../../utils/token'
+import useRequest from '../../hooks/useRequest'
 
 const AuthProvider = ({children}) => {
 
-
-    useEffect(() => {
-        getUserInformationFromAccessToken()
-    }, [])
+    const [isAuth, setIsAuth] = useState(false)
+    const [userId, setUserId] = useState()
+    const [username, setUsername] = useState("")
 
 
     const contextData = {
-        isAuth: false,
-        user_id: 0,
-        username: ""
+        isAuth,
+        setIsAuth,
+        userId,
+        setUserId,
+        username,
+        setUsername
     }
-  
+
+    const [isLoading, checkLogin] = useRequest(async () =>{
+        const token = getAccessToken()
+
+        
+        const response = await AuthService.verify(token)
+
+        if(response.correct){
+            const userData = getUserInformationFromAccessToken()
+            setUsername(userData.username)
+            setUserId(userData.userId)
+            setIsAuth(response.correct)
+        }
+
+    })
+
+
+    useEffect(() => {
+        checkLogin()
+    }, [])
+
+
+    if(isLoading)
+        return 'Loading'
+
+
     return (
-      <AuthContext.Provider value={contextData}>
+      <AuthContext.Provider value={{contextData}}>
           {children}
       </AuthContext.Provider>
     )
