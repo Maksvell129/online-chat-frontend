854a569c72889612dc7f2aae171cf9185ae818c4
Ash1VT <nikita.panasevitch@yandex.by>
2023-04-13 16:45:51 +0300

    moved routes to AppRouter file

---
+++ chat/src/AppRouter.js
@@ -0,0 +1,32 @@
+import React, {useContext} from 'react'
+import AuthContext from "./contexts/auth/AuthContext";
+import {BrowserRouter, Navigate, Route, Routes} from "react-router-dom";
+import { privateRoutes, publicRoutes } from './routes';
+
+const AppRouter = () => {
+
+    const {contextData} = useContext(AuthContext)
+
+    return (
+        <BrowserRouter>{
+            contextData.isAuth
+                ?
+                <Routes>
+                    {privateRoutes.map(route => 
+                        <Route element={route.element} path={route.path} key={route.path}/>
+                    )}
+                    <Route path='*' element={<Navigate to="chat"/>}></Route>
+                </Routes>
+                :
+                <Routes>    
+                    {publicRoutes.map(route => 
+                        <Route element={route.element} path={route.path} key={route.path}/>
+                    )}
+                    <Route path='*' element={<Navigate to="login"/>}></Route>
+                </Routes>
+            }
+        </BrowserRouter>
+    )
+}
+
+export default AppRouter
