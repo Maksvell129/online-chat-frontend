b6fcfcbf3f56c6aa8b92b7eb2468608ee4e0c655
Egor Grischuk <egorfog@gmail.com>
2023-04-14 11:53:43 +0300

    Fix modal window 2.0

--- chat/src/components/Users/UsersList.js
+++ chat/src/components/Users/UsersList.js
@@ -1,19 +1,22 @@
 import './Users.css';
 
-const UsersList = ({ onClose }) => (
-  <div className={'modalContainer'}>
-    <div className="close-users-btn">
-      <button onClick={onClose} className="close-users-bnt-style"><img width={10} src="images/close.png"/></button>
+const UsersList = ({onClose}) => (
+    <div className={'modalBg'}>
+        <div className={'modalContainer'}>
+            <div className="close-users-btn">
+                <button onClick={onClose} className="close-users-bnt-style"><img width={10} src="images/close.png"/>
+                </button>
+            </div>
+            <div className="abel-normal-black-16px">
+                <dl>
+                    <dd>Bebra</dd>
+                    <dd>Bebra</dd>
+                    <dd>Bebra</dd>
+                    <dd>Bebra</dd>
+                </dl>
+            </div>
+        </div>
     </div>
-      <div className="abel-normal-black-16px">
-          <dl>
-              <dd>Bebra</dd>
-              <dd>Bebra</dd>
-              <dd>Bebra</dd>
-              <dd>Bebra</dd>
-          </dl>
-      </div>
-  </div>
 );
 
 export default UsersList;
