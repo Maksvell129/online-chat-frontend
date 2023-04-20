f440f65e12c85b6de54ff1569147763bdb249957
Ash1VT <nikita.panasevitch@yandex.by>
2023-04-21 00:45:49 +0300

    added component with inputing message and fixed adaptive layout problem with 'height: 1px'

--- chat/src/pages/ChatPage.js
+++ chat/src/pages/ChatPage.js
@@ -1,18 +1,44 @@
-import React, {useState} from 'react'
+import React, {useState, useRef} from 'react'
 import BarHeader from '../components/BarHeader/BarHeader';
-import '../components/Chat/Chat.css';
-import Chat from '../components/Chat/Chat';
+import MessagesList from '../components/MessagesList/MessagesList';
 import UsersList from "../components/Users/UsersList";
+import CustomInput from '../components/MessageInput/MessageInput';
+import '../components/MessagesList/MessagesList.css'
 
 const ChatPage = () => {
     const [isInfoOpened, setIsInfoOpened] = useState(false);
+    
+    const message = useRef();
+
+    const [messages, setMessages] = useState([]);
+    
+    
+    const handleSendMessage = () => {
+        const stringMessage = message.current.value
+        if (stringMessage) {
+            // sendMessage(stringMessage);
+            setMessages([...messages, stringMessage]);
+            message.current.value = ""
+        }
+    };
+
+    const handleKeyDown = (event) => {
+        if (event.key === "Enter") {
+            handleSendMessage();
+        }
+    };
 
     return (
      <div className="container-center-horizontal">
-         <div className="x4 screen chat-content">
+         <div className="x4 screen">
              <BarHeader spanText="Chat" headerTitle="Chat" handleUsersClick={() => setIsInfoOpened(true)}/>
              {isInfoOpened && <UsersList onClose={() => setIsInfoOpened(false)} />}
-             <Chat/>
+             <MessagesList messages={messages}/>
+             <CustomInput
+                onSubmit={handleSendMessage}
+                onKeyDown={handleKeyDown}
+                messageRef={message}
+            />
          </div>
      </div>
     )
