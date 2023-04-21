c03d6cbf2bd49c465c755c31f863ffbb80aea152
Ash1VT <nikita.panasevitch@yandex.by>
2023-04-21 18:22:37 +0300

    fixed modal window location

--- chat/src/components/Users/UsersList.js
+++ chat/src/components/Users/UsersList.js
@@ -4,11 +4,14 @@ const UsersList = ({onClose}) => (
     <div className={'modalBg'}>
         <div className={'modalContainer'}>
             <div className="close-users-btn">
-                <button onClick={onClose} className="close-users-bnt-style"><img width={10} src="images/close.png"/>
+                <button onClick={onClose} className="close-users-bnt-style">
+                    <img width={10} src="images/close.png"/>
                 </button>
             </div>
             <div className="abel-normal-black-16px">
-                <dl>
+                <dl className="users">
+                    <dd>Bebra</dd>
+                    <dd>Bebra</dd>
                     <dd>Bebra</dd>
                     <dd>Bebra</dd>
                     <dd>Bebra</dd>
