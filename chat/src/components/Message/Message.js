e674e6ee6f7fc681d745e06806d59ca1a6ef598a
Ash1VT <nikita.panasevitch@yandex.by>
2023-04-30 21:29:53 +0300

    added message's options context menu and callbacks for this context menu

--- chat/src/components/Message/Message.js
+++ chat/src/components/Message/Message.js
@@ -1,10 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import "../Message/Message.css";
+import MessageOptions from "../MessageOptions/MessageOptions";
 
-function Message({username, text, time, isOwnMessage}) {
+function Message({username, text, time, isOwnMessage, onStartEdit, onDelete}) {
+
+    const [isContextMenuVisible, setIsContextMenuVisible] = useState(false)
+
+    const onContextMenu = (event) => {
+        event.preventDefault()
+        setIsContextMenuVisible(true)
+    }
 
     return (
-        <div className={isOwnMessage ? "MyMessage" : "ForeignMessage"}>
+        <div className={isOwnMessage ? "MyMessage" : "ForeignMessage"} onContextMenu={onContextMenu}>
             <div className="abel-normal-black-18px UserName">
                 {username}
             </div>
@@ -14,6 +22,12 @@ function Message({username, text, time, isOwnMessage}) {
             <div className="abel-normal-black-8px SendTime">
                 {time}
             </div>
+            {isContextMenuVisible && isOwnMessage && 
+                <MessageOptions onClose={() => setIsContextMenuVisible(false)} 
+                                onMessageStartEdit={() => {onStartEdit();
+                                                           setIsContextMenuVisible(false)}}
+                                onMessageDelete={() => {onDelete();
+                                                        setIsContextMenuVisible(false)}}/>}
         </div>
     );
 }
