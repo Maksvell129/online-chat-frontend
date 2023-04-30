658000d03c97a56b41a9bfffad72335f6a225277
Ash1VT <nikita.panasevitch@yandex.by>
2023-04-30 21:31:05 +0300

    added callbacks for starting editing message and deleting message

--- chat/src/components/MessagesList/MessagesList.js
+++ chat/src/components/MessagesList/MessagesList.js
@@ -2,12 +2,24 @@ import React, {useState, useRef} from "react";
 import Message from "../Message/Message";
 import "./MessagesList.css";
 
-function MessagesList({messages}) {
+function MessagesList({messages, handleMessageStartEdit, handleMessageDelete}) {
 
     return (
         <div className="messages-list-container">
             <ul className="message-style">
-                 {messages.map((message, index) => (<Message key={index} username={message.author} text={message.text} time={message.time} isOwnMessage={message.isOwnMessage}/>))}
+                {
+                    messages.map((message, index) => (
+                        <Message 
+                            key={message.id} 
+                            username={message.author} 
+                            text={message.text} 
+                            time={message.time} 
+                            isOwnMessage={message.isOwnMessage}
+                            onStartEdit={() => handleMessageStartEdit(message.id)}
+                            onDelete={() => handleMessageDelete(message.id)}
+                            />)
+                    )
+                }
             </ul>
         </div>
     );
