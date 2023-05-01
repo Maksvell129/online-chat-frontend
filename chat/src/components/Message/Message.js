f262876fa4ba0333a0ea20567a596003cf14034a
Ash1VT <nikita.panasevitch@yandex.by>
2023-05-01 17:49:51 +0300

    added modified text if message was modified

--- chat/src/components/Message/Message.js
+++ chat/src/components/Message/Message.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import "../Message/Message.css";
 import MessageOptions from "../MessageOptions/MessageOptions";
 
-function Message({username, text, time, isOwnMessage, onStartEdit, onDelete}) {
+function Message({username, text, time, isModified, isOwnMessage, onStartEdit, onDelete}) {
 
     const [isContextMenuVisible, setIsContextMenuVisible] = useState(false)
 
@@ -19,8 +19,16 @@ function Message({username, text, time, isOwnMessage, onStartEdit, onDelete}) {
             <div className="abel-normal-black-14px Text">
                 {text}
             </div>
-            <div className="abel-normal-black-8px SendTime">
-                {time}
+            <div className="message-additional-info">
+                {isModified && 
+                <div className="abel-normal-gray-12px Modified">
+                    modified
+                </div>
+                }
+                
+                <div className="abel-normal-black-8px SendTime">
+                    {time}
+                </div>
             </div>
             {isContextMenuVisible && isOwnMessage && 
                 <MessageOptions onClose={() => setIsContextMenuVisible(false)} 
