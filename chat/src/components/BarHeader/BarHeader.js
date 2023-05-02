5df8ae26a16f2e79525581d5f017010ad6ec6a72
Ash1VT <nikita.panasevitch@yandex.by>
2023-05-02 10:43:17 +0300

    added showing buttons only for authenticated user

--- chat/src/components/BarHeader/BarHeader.js
+++ chat/src/components/BarHeader/BarHeader.js
@@ -1,15 +1,32 @@
 import './BarHeader.css'
-import React from "react";
+import React, { useContext } from "react";
+import AuthContext from '../../contexts/auth/AuthContext';
+import { removeAccessToken, removeRefreshToken } from '../../utils/token';
+
 
 function BarHeader({ spanText, headerTitle, handleUsersClick }) {
+
+    const {authContextData} = useContext(AuthContext)
+
+    const logout = (event) => {
+        removeAccessToken()
+        removeRefreshToken()
+        authContextData.setIsAuth(false)
+        authContextData.setUsername()
+        authContextData.setUserId()
+    }
+
     return (
         <div className="bar-header">
+            
             <div className="return-btn">
-                <button className="return-btn-style">
+                {authContextData.isAuth && 
+                <button className="return-btn-style" onClick={logout}>
                     <img width={15} src="images/return.png" alt={'Return'}/>
                 </button>
+                }
             </div>
-            <div className="collumn-chat">
+            <div className="column-chat">
                 <div className="header-title abel-normal-black-16px">
                     <span className="abel-normal-black-16px">{spanText}</span>
                 </div>
@@ -18,9 +35,11 @@ function BarHeader({ spanText, headerTitle, handleUsersClick }) {
                 </div>
             </div>
             <div className={'users-btn'}>
+                {authContextData.isAuth && 
                 <button className="users-btn-style" onClick={handleUsersClick}>
                     <img width={20} src="images/users.png" alt={'Users'}/>
                 </button>
+                }
             </div>
         </div>
     );
