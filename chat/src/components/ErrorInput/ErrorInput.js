f3282c19befcf7cb4aa7ccbed31321769b207031
Ash1VT <nikita.panasevitch@yandex.by>
2023-05-02 10:44:01 +0300

    removed using ref

--- chat/src/components/ErrorInput/ErrorInput.js
+++ chat/src/components/ErrorInput/ErrorInput.js
@@ -1,17 +1,17 @@
 import React from 'react'
 import './ErrorInput.css'
 
-const ErrorInput = ({defaultText, errorText, innerRef, ...props}) => {
+const ErrorInput = ({errorText, ...props}) => {
     if(errorText)
         return (
             <div>
-                <input {...props} ref={innerRef} value={defaultText}/>
+                <input {...props}/>
                 <p className='error-text abel-normal-red-14px'>{errorText}</p>
             </div>
         )
         
     return (
-        <input {...props} ref={innerRef} value={defaultText}/>
+        <input {...props}/>
     )
         
 }
